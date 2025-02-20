d68e949a44e13288eb8e5b766676ec9cea9df696
Alessandro-Franca01 <alessandro0564@yahoo.com.br>
2025-02-20 16:10:20 -0300

    feat: Add feature to share list shopping between users

--- routes/api.php
+++ routes/api.php
@@ -5,16 +5,6 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\UserController;
 use App\Http\Controllers\API\ListShoppingController;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
@@ -23,3 +13,5 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 Route::apiResource('users', UserController::class);
 Route::apiResource('lists', ListShoppingController::class)->except(['store']);
 Route::apiResource('users.lists', ListShoppingController::class)->only(['store', 'index'])->scoped();
+Route::post('/lists/{list}/share', [ListShoppingController::class, 'share']);
+Route::post('/lists/{list}/unshare', [ListShoppingController::class, 'unshare']);
