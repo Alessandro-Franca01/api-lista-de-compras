253123412be9cb650bf06f38747fb613847ee17d
Alessandro-Franca01 <alessandro0564@yahoo.com.br>
2025-04-25 03:18:21 -0300

    feat: Create page to privacy policy and refactore README file

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/politica-de-privacidade', function () {
+    return view('privacy_policy');
+});
